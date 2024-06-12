62d12ec944d061e083363a861f89724b45f69cb0
nik-nul <88674214+nik-nul@users.noreply.github.com>
2024-06-12 22:34:13 +0800

    mkdir

--- checkout.py
+++ checkout.py
@@ -7,6 +7,8 @@ raw_date = time.localtime()
 y, m, d = raw_date.tm_year, raw_date.tm_mon, raw_date.tm_mday
 date = f"{y}.{m}.{d}"
 
+os.system('mkdir output')
+
 if len(sys.argv) > 1:
     if sys.argv[1] != 'test':
         os.system(f'mv ./{sys.argv[1]}/* ./output/')
